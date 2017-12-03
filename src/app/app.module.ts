1e4779f1b5bd42797f4ec8dd0f8cde8595a64ff0
Antonio <antonio.pavlov97@gmail.com>
2017-12-03 17:16:40 +0200

    Start the project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component'
+import { NavigationComponent } from './components/navigation/navigation.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
